0c1c48d56e1ee9ca8d0ea787f489e5889c7fe281
Việt Anh Bùi <92940157+vietanhbui2000@users.noreply.github.com>
2022-07-14 01:29:20 +0700

    Create uptodown_download.sh

---
+++ uptodown_download.sh
@@ -0,0 +1,21 @@
+declare -A apks
+
+apks["com.google.android.youtube.apk"]="https://youtube.en.uptodown.com/android/apps/16906/versions"
+apks["com.google.android.apps.youtube.music.apk"]="https://youtube-music.en.uptodown.com/android/apps/146929/versions"
+
+get_apk_download_url()
+{
+    version_url=$(curl -s $1 | jq ".data[] | select(.version | contains(\"$2\")) | .versionURL")
+    dl_url=$(curl -s ${version_url:1:-1} | grep -oE "https:\/\/dw\.uptodown\.com.+\/")
+    echo $dl_url
+}
+
+for apk in "${!apks[@]}"
+do
+    if [ ! -f $apk ]
+    then
+        echo "Downloading $apk"
+        version=$(cat youtube_versions.json | jq -r ".\"$apk\"")
+        curl -sLo $apk $(get_apk_download_url ${apks[$apk]} $version)
+    fi
+done
