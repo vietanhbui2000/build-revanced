fd625d49950dede2506a9eba600b9c23c095ac2d
Việt Anh Bùi <92940157+vietanhbui2000@users.noreply.github.com>
2022-07-27 14:10:18 +0700

    Update compile_revanced.sh

--- compile_revanced.sh
+++ compile_revanced.sh
@@ -2,9 +2,16 @@
 
 VMG_VERSION="0.2.24.220220"
 
+patches_file=./revanced_patches.md
+
+excluded_start="$(grep -n -m1 'EXCLUDE PATCHES' "$patches_file" | cut -d':' -f1)"
+included_start="$(grep -n -m1 'INCLUDE PATCHES' "$patches_file" | cut -d':' -f1)"
+
+excluded_patches="$(tail -n +$excluded_start $patches_file | head -n "$(( included_start - excluded_start ))" | grep '^[^#]')"
+included_patches="$(tail -n +$included_start $patches_file | grep '^[^#]')"
+
 echo "Declaring variables and their attributes"
-revanced_patches=./revanced_patches.md
-declare -a excluded_patches
+declare -a patches
 declare -A artifacts
 
 artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli .jar"
@@ -18,6 +25,14 @@ get_artifact_download_url()
     api_url="https://api.github.com/repos/$1/releases/latest"
     result=$(curl -s $api_url | jq ".assets[] | select(.name | contains(\"$2\") and contains(\"$3\") and (contains(\".sig\") | not)) | .browser_download_url")
     echo "${result:1:-1}"
+}
+
+populate_patches()
+{
+    while read -r revanced_patches
+    do
+        patches+=("$1 $revanced_patches")
+    done <<< "$2"
 }
 
 echo "Cleaning up"
@@ -46,26 +61,22 @@ then
     mv com.mgoogle.android.gms@$VMG_VERSION.apk vanced-microG.apk
 fi
 
+[[ ! -z "$excluded_patches" ]] && populate_patches "-e" "$excluded_patches"
+[[ ! -z "$included_patches" ]] && populate_patches "-i" "$included_patches"
+
 echo "Preparing"
 mkdir -p build
-if grep -q '^[^#]' $revanced_patches
-then
-    while read -r patches
-    do
-        excluded_patches+=("-e $patches")
-    done < <(grep '^[^#]' $revanced_patches)
-fi
 
 echo "Compiling YouTube"
 if [ -f "com.google.android.youtube.apk" ]
 then
     echo "Compiling root package"
     java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar --mount \
-                               -e microg-support ${excluded_patches[@]} \
+                               -e microg-support ${patches[@]} \
                                -a com.google.android.youtube.apk -o build/revanced-root.apk
     echo "Compiling non-root package"
     java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar \
-                               ${excluded_patches[@]} \
+                               ${patches[@]} \
                                -a com.google.android.youtube.apk -o build/revanced-nonroot.apk
 else
     echo "Cannot find YouTube base package, skip compiling"
@@ -76,11 +87,11 @@ if [ -f "com.google.android.apps.youtube.music.apk" ]
 then
     echo "Compiling root package"
     java -jar revanced-cli.jar -b revanced-patches.jar --mount \
-                               -e microg-support ${excluded_patches[@]} \
+                               -e microg-support ${patches[@]} \
                                -a com.google.android.apps.youtube.music.apk -o build/revanced-music-root.apk
     echo "Compiling non-root package"
     java -jar revanced-cli.jar -b revanced-patches.jar \
-                               ${excluded_patches[@]} \
+                               ${patches[@]} \
                                -a com.google.android.apps.youtube.music.apk -o build/revanced-music-nonroot.apk
 else
     echo "Cannot find YouTube Music base package, skip compiling"
