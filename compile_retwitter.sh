040c6060b6821d35bcf1433495696066c93193c3
Việt Anh Bùi <92940157+vietanhbui2000@users.noreply.github.com>
2022-08-03 19:22:04 +0700

    Update compile_retwitter.sh

--- compile_retwitter.sh
+++ compile_retwitter.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-TWITTER_VERSION="9.53.0-release.0"
-
 echo "Declaring variables"
 declare -A artifacts
 
 artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli .jar"
 artifacts["revanced-patches.jar"]="revanced/revanced-patches revanced-patches .jar"
-artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
 
 get_artifact_download_url()
 {
@@ -33,15 +30,6 @@ do
         curl -sLo "$artifact" $(get_artifact_download_url ${artifacts[$artifact]})
     fi
 done
-
-echo "Fetching Twitter"
-chmod +x apkeep
-if [ ! -f "com.twitter.android.apk" ]
-then
-    echo "Downloading Twitter"
-    ./apkeep -a com.twitter.android@${TWITTER_VERSION} .
-    mv com.twitter.android@${TWITTER_VERSION}.apk com.twitter.android.apk
-fi
 
 echo "Preparing"
 mkdir -p retwitter
