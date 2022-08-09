44f33b5631041fbfdeb56fbba919e4bc6e844a74
Việt Anh Bùi <92940157+vietanhbui2000@users.noreply.github.com>
2022-08-10 03:00:43 +0700

    Update youtube_download.sh

--- revanced/youtube_download.sh
+++ revanced/youtube_download.sh
@@ -3,14 +3,72 @@
 echo "Declaring variables"
 declare -A apks
 
-apks["com.google.android.youtube.apk"]="https://youtube.en.uptodown.com/android/apps/16906/versions"
-apks["com.google.android.apps.youtube.music.apk"]="https://youtube-music.en.uptodown.com/android/apps/146929/versions"
+apks["com.google.android.youtube.apk"]=dl_youtube
+apks["com.google.android.apps.youtube.music.apk"]=dl_youtube-music
 
-get_apk_download_url()
-{
-    version_url=$(curl -s "$1" | jq ".data[] | select(.version | contains(\"$2\")) | .versionURL")
-    dl_url=$(curl -s "${version_url:1:-1}" | grep -oE "https:\/\/dw\.uptodown\.com.+\/")
-    echo "$dl_url"
+WGET_HEADER="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
+ARM_V7A="arm-v7a"
+ARM64_V8A="arm64-v8a"
+
+req() { wget -nv -O "$2" --header="$WGET_HEADER" "$1"; }
+
+get_apk_vers() { req "$1" - | sed -n 's;.*Version:</span><span class="infoSlide-value">\(.*\) </span>.*;\1;p'; }
+
+get_largest_ver() {
+	local max=0
+	while read -r v || [ -n "$v" ]; do
+		if [[ ${v//[!0-9]/} -gt ${max//[!0-9]/} ]]; then max=$v; fi
+	done
+	if [[ $max = 0 ]]; then echo ""; else echo "$max"; fi
+}
+
+dl_apk() {
+	local url=$1 regexp=$2 output=$3
+	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n "s/href=\"/@/g; s;.*${regexp}.*;\1;p")"
+	echo "$url"
+	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+	url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
+	req "$url" "$output"
+}
+
+dl_youtube() {
+	echo "Downloading YouTube"
+	local last_ver
+	last_ver="$version"
+	last_ver="${last_ver:-$(get_apk_vers "https://www.apkmirror.com/uploads/?appcategory=youtube" | get_largest_ver)}"
+
+	echo "Choosing version '${last_ver}'"
+	local base_apk="com.google.android.youtube.apk"
+	if [ ! -f "$base_apk" ]; then
+		declare -r dl_url=$(dl_apk "https://www.apkmirror.com/apk/google-inc/youtube/youtube-${last_ver//./-}-release/" \
+			"APK</span>[^@]*@\([^#]*\)" \
+			"$base_apk")
+		echo "YouTube version: ${last_ver}"
+		echo "downloaded from: [APKMirror - YouTube]($dl_url)"
+	fi
+}
+
+dl_youtube-music() {
+	local arch=$ARM64_V8A
+	echo "Downloading YouTube Music (${arch})"
+	local last_ver
+	last_ver="$version"
+	last_ver="${last_ver:-$(get_apk_vers "https://www.apkmirror.com/uploads/?appcategory=youtube-music" | get_largest_ver)}"
+
+	echo "Choosing version '${last_ver}'"
+	local base_apk="com.google.android.apps.youtube.music.apk"
+	if [ ! -f "$base_apk" ]; then
+		if [ "$arch" = "$ARM64_V8A" ]; then
+			local regexp_arch='arm64-v8a</div>[^@]*@\([^"]*\)'
+		elif [ "$arch" = "$ARM_V7A" ]; then
+			local regexp_arch='armeabi-v7a</div>[^@]*@\([^"]*\)'
+		fi
+		declare -r dl_url=$(dl_apk "https://www.apkmirror.com/apk/google-inc/youtube-music/youtube-music-${last_ver//./-}-release/" \
+			"$regexp_arch" \
+			"$base_apk")
+		echo "\nYouTube Music (${arch}) version: ${last_ver}"
+		echo "downloaded from: [APKMirror - YouTube Music ${arch}]($dl_url)"
+	fi
 }
 
 for apk in "${!apks[@]}"
@@ -19,6 +77,6 @@ do
     then
         echo "Downloading $apk"
         version=$(jq -r ".\"$apk\"" <youtube_versions.json)
-        curl -sLo $apk "$(get_apk_download_url ${apks[$apk]} "$version")"
+        ${apks[$apk]}
     fi
 done
