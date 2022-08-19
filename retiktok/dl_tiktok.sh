b0606e1d8ac6e8e64662afa49db15ca3f31764d2
Việt Anh Bùi <92940157+vietanhbui2000@users.noreply.github.com>
2022-08-19 18:16:35 +0700

    Update and rename tiktok_download.sh to dl_tiktok.sh

--- retiktok/dl_tiktok.sh
+++ retiktok/dl_tiktok.sh
@@ -62,7 +62,7 @@ do
     if [ ! -f $apk ]
     then
         echo "Downloading $apk"
-        version=$(jq -r ".\"$apk\"" <tiktok_version.json)
+        version=$(jq -r ".\"$apk\"" <dl_tiktok-version.json)
         ${apks[$apk]}
     fi
 done
